66d9c68cd98abfaadf0f087cc8a6e09d077d4405
Nilesh <nkc10800@gmail.com>
2023-11-28 05:13:12 +0530

    Added registration and login form, also added registration authentication

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name = 'home'),
+    path('login/', login_page, name = 'login_page'),
+    path('register/', register_page, name = 'register_page'),
     path('about/', about, name = 'about'),
     path('contact/', contact, name = 'contact'),
     path('recipes/', recipes, name = 'recipes'),
